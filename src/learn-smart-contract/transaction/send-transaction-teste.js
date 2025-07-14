40a4cdfd0c7a3fae0a8cc2adb1c6034ec20ab703
luis-gustavoBP <lg.poca@unesp.br>
2025-07-14 12:49:32 -0300

    Alterando o projeto para etherjs

--- src/learn-smart-contract/transaction/send-transaction-teste.js
+++ src/learn-smart-contract/transaction/send-transaction-teste.js
@@ -3,11 +3,11 @@ import { LegacyTx } from '@ethereumjs/tx';
 import { Common, Hardfork, Chain } from '@ethereumjs/common';
 import { Buffer } from 'buffer';
 
-const web3 = new Web3('https://sepolia.infura.io/v3/SEU_TOKEN_INFURA');
+const web3 = new Web3('');
 
-const addressFrom = '0x8Ec28ff5aD0cACd0e610218622328Cb22f8F2BbE';
-const privateKey = Buffer.from('5a474bfde441ede9d73ce4ff316a5f429c7ca89bc1b32d26a93fd2cbe1d0c601', 'hex');
-const addressTo = '0x3587F4FfD2C8cf89040d1e0E301378402F7E8bAE';
+const addressFrom = '';
+const privateKey = Buffer.from('', 'hex');
+const addressTo = '';
 
 const main = async () => {
     const txCount = await web3.eth.getTransactionCount(addressFrom);
